61610a1b0516857e951c599a327b3969a7a186c9
Ekaterina Nikonova <u0441u@gmail.com>
2021-09-12 15:30:31 +0200

    add solution for 1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }else{
+    for(let i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }
 }
