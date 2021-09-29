7882ddb4c7540bf0aca92929d029250153e0b4fd
Ekaterina Nikonova <u0441u@gmail.com>
2021-09-29 20:12:59 +0400

    solution 3 module 5 task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let strings = str.split(' ');
+  let nums = strings
+      .filter((value) => isFinite(value))
+      .map((value) => +value);
+  nums.sort((a, b) => {
+    if (a > b) {return 1;}
+    return a < b ? -1 : 0;
+  });
+  let lastElementIndex = nums.length - 1;
+  let max = nums[lastElementIndex];
+  let min = nums[0];
+  return {min, max};
 }
